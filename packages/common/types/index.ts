2e13f67ed8af4687f44c4f7eda6b711d3375ab88
hanuvadlamudi <hanuvadlamudivhr@gmail.com>
2026-07-30 09:23:06 +0530

    connect with db & implement sign up

---
+++ packages/common/types/index.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const SignUpSchema = z.object({
+  username: z.string().min(3).max(30),
+  password: z.string().min(8).max(12),
+});
+
+export type SignUpInput = z.infer<typeof SignUpSchema>;
